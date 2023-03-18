657a3b63e21a276e5123339c46e331c3bf745a43
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 21:31:16 +0300

    Update TemperatureConverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
+    public static void main(String[] args) {  
+        TemperatureConverter n = new TemperatureConverter();
+        n.toFahrenheit(11);
+    }
+    
     public void toFahrenheit(int temperatureCelsius){
+        double d = (double)temperatureCelsius;
+        int i = (int)(((d * 9 / 5) + 32)*10);
+        double dd = (double)i/10;
+        System.out.println(dd);
     }
 }
