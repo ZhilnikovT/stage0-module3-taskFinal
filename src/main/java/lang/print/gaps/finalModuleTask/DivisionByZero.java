c2216d4475a54c8d77d8b6a2ba935e17eda87028
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 21:34:42 +0300

    Update DivisionByZero.java

--- src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DivisionByZero.java
@@ -1,15 +1,6 @@
 package lang.print.gaps.finalModuleTask;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class DivisionByZero {
     public static void main(String[] args) {
