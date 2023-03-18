2be04503c4073120f20f8235b64c7c50ab9fe428
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 21:07:50 +0300

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.finalModuleTask;
 
+
 public class DigitsSumCalculator {
+     public static void main(String[] args) {  
+        DigitsSumCalculator n = new DigitsSumCalculator();
+        n.calculateSum(100);
+    }
+    
     public void calculateSum(int number) {
+        char[] chars = ("" + number).toCharArray();
+        int rev = Integer.parseInt(String.valueOf(chars[3])) + Integer.parseInt(String.valueOf(chars[2]))
+                + Integer.parseInt(String.valueOf(chars[1])) + Integer.parseInt(String.valueOf(chars[0]));
+        System.out.println(rev);
     }
 }
