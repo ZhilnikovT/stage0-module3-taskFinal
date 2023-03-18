5780f5749bedc9a5748d0d240da46992580c2bf3
ZhilnikovT <126979946+ZhilnikovT@users.noreply.github.com>
2023-03-18 20:58:26 +0300

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
+    public static void main(String[] args) {  
+        NumberReverter n = new NumberReverter();
+       
+    }
+    
     public void revert(int number) {
+        char[] chars = ("" + number).toCharArray();
+        String rev = String.valueOf(chars[2]) + String.valueOf(chars[1]) + String.valueOf(chars[0]) ;
+       
+        System.out.println(Integer.parseInt(rev));
     }
 }
